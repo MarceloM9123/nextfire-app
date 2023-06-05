12bf759a29b73b6d3c5654b067df34a898a36dfd
marcelo <marcelomata91@gmail.com>
2023-06-05 18:24:06 -0500

    update headers for cors error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "https://www.marcelomata.com",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin", value: "https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/hdris/empty-wharehouse/empty_warehouse_01_1k.hdr" 
+          }
+        ]
+      }
+    ]
+  }
   
 }
 
