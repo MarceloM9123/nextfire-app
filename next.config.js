540ca42a73183a9b4449180e9c668c60e5d432a9
marcelo <marcelomata91@gmail.com>
2023-06-06 00:10:14 -0500

    update headers cors wc

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       {
         source: "/:path*",
         headers: [
-          {key: "Access-Control-Allow-Origin", value: "https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/hdris/empty-wharehouse/empty_warehouse_01_1k.hdr"}
+          {key: "Access-Control-Allow-Origin", value: "*"}
         ]
       }
     ]
