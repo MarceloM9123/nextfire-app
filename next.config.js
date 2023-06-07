79f0d26fcfa02ea067a424ed7fdcdf7c5a57e35f
marcelo <marcelomata91@gmail.com>
2023-06-06 23:41:32 -0500

    Revert "update headers allow origin empty-warehouse assets"
    This reverts commit fcdb534c6aa92599c3bf7c27a5a1529439b6d32d.

--- next.config.js
+++ next.config.js
@@ -6,10 +6,7 @@ const nextConfig = {
       {
         source: "/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          {key: "Access-Control-Allow-Origin", value: "https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/hdris/empty-wharehouse/empty_warehouse_01_1k.hdr"}
         ]
       }
     ]
